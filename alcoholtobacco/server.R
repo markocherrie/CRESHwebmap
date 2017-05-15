4c4bfa09e83a543daf84f16e58edcbb29dd75849
markocherrie <mark.cherrie.mc@gmail.com>
2017-05-15 14:56:21 +0000

    Added RJSONIO:: to geocoder to fix

--- alcoholtobacco/server.R
+++ alcoholtobacco/server.R
@@ -56,7 +56,7 @@ BING <- function(str){
   require(RJSONIO)
   u <- URLencode(paste0("http://dev.virtualearth.net/REST/v1/Locations?q=", str, "&maxResults=1&key=Apo4HssxpmYvVbDEUA464pmX5Y30xsQNlJ4pES6Z6D056puS63D90MLZlQ1yVeTG"))
   d <- getURL(u)
-  j <- fromJSON(d,simplify = FALSE) 
+  j <- RJSONIO::fromJSON(d,simplify = FALSE) 
   if (j$resourceSets[[1]]$estimatedTotal > 0) {
     lat <- j$resourceSets[[1]]$resources[[1]]$point$coordinates[[1]]
     lng <- j$resourceSets[[1]]$resources[[1]]$point$coordinates[[2]]
